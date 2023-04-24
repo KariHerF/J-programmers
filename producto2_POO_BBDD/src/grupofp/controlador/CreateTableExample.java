b5cc317ead00bb72aab612a9268171ba30d581fb
Joan Bogunya Salvans <joanbogunya98@gmail.com>
2023-04-24 12:13:56 +0200

    updated example of table creation

--- producto2_POO_BBDD/src/grupofp/controlador/CreateTableExample.java
+++ producto2_POO_BBDD/src/grupofp/controlador/CreateTableExample.java
@@ -1,32 +1,39 @@
 package grupofp.controlador;
+
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
 public class CreateTableExample {
-	 public static void main(String[] args) {
 
-	      // Obtener la conexión a la base de datos
-	      try (Connection conexion = Conexion.getConnection()) { //Aqui no me detecta quizás que tengo el java.sql.Connection
+	public static void main(String[] args) {
 
-	         // Crear una sentencia
-	         Statement sentencia = conexion.createStatement();
+		// Obtener la conexión a la base de datos
+		try (Connection conexion = ConexionDB.conectar()) { // He importado la conexion de ConexionDB.java e
+															// inicializado esa
 
-	         // Definir la sentencia SQL para crear una tabla (esta es de ejemplo)
-	         String sql = "CREATE TABLE articulos " +
-	            "(id INT AUTO_INCREMENT, " +
-	            "codigo VARCHAR(50) NOT NULL, " +
-	            "descripcion VARCHAR(50) NOT NULL,"+
-	            "pvp DOUBLE NOT NULL,"+
-	            "tiempoPrep DOUBLE NOT NULL,"+
-	            	"gastosEnvio DOUBLE NOT NULL)";
-	         // Ejecutar la sentencia SQL
-	         sentencia.executeUpdate(sql);
-	         System.out.println("Tabla creada correctamente.");
+			// Crear una sentencia
+			Statement sentencia = conexion.createStatement();
 
-	      } catch (SQLException e) {
-	         e.printStackTrace();
-	      }
-	   }
+			// Definir la sentencia SQL para crear la tabla de articulos
+			String sql = "CREATE TABLE IF NOT EXISTS articulos " +
+					"(id INT AUTO_INCREMENT, " +
+					"codigo VARCHAR(50) NOT NULL, " +
+					"descripcion VARCHAR(50) NOT NULL," +
+					"pvp DOUBLE NOT NULL," +
+					"tiempoPrep DOUBLE NOT NULL," +
+					"gastosEnvio DOUBLE NOT NULL)";
+			// Ejecutar la sentencia SQL
+			try {
+				sentencia.executeUpdate(sql);
+				System.out.println("Tabla creada correctamente.");
+			} catch (SQLException e) {
+				System.out.println("Ha ocurrido el siguiente error al crear la tabla articulos: " + e.getMessage());
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
 
 }
