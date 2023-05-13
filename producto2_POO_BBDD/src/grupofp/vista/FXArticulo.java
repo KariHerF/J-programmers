7153ed11f914fba469fd660e4537131d61febcb4
Joan Bogunya Salvans <joanbogunya98@gmail.com>
2023-05-13 18:19:17 +0200

    menu articulos

--- producto2_POO_BBDD/src/grupofp/vista/FXArticulo.java
+++ producto2_POO_BBDD/src/grupofp/vista/FXArticulo.java
@@ -2,9 +2,12 @@ package grupofp.vista;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -16,9 +19,47 @@ public class FXArticulo extends FXMain {
 
     public void start(Stage mainMenu) {
 
-        Button btn = new Button();
-        btn.setText("Articulo");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        Text titulo = new Text(30, 30, "¿Que deseas hacer?");
+        titulo.setStyle("-fx-font: 15 arial;");
+        
+        Button addArticulo = new Button();
+        addArticulo.setText("Añadir articulo");
+        addArticulo.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                pantallaAddArticulo();
+                articuloStage.hide();
+
+            }
+        });
+        
+        Button showArticulos = new Button();
+        showArticulos.setText("Mostrar articulos");
+        showArticulos.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                pantallaShowArticulos();
+                articuloStage.hide();
+
+            }
+        });
+        
+        Button deleteArticulo = new Button();
+        deleteArticulo.setText("Eliminar articulo");
+        deleteArticulo.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                pantallaDeleteArticulo();
+                articuloStage.hide();
+            }
+        });
+        
+        Button volver = new Button();
+        volver.setText("Volver");
+        volver.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
@@ -28,13 +69,99 @@ public class FXArticulo extends FXMain {
         });
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(titulo, addArticulo, showArticulos, deleteArticulo, volver);
+        vbox.setAlignment(Pos.CENTER);
+        root.getChildren().add(vbox);
         
         Scene scene = new Scene(root, 300, 250);
         
-        articuloStage.setTitle("Hello World!");
+        articuloStage.setTitle("Articulos");
         articuloStage.setScene(scene);
         articuloStage.show();
+    }
+    
+    private void pantallaAddArticulo() {
+        Stage addArticuloStage = new Stage();
+        StackPane root = new StackPane();
+        Text titulo = new Text(30, 30, "Añadir articulo");
+        titulo.setStyle("-fx-font: 15 arial;");
+        
+        Button volver = new Button();
+        volver.setText("Volver");
+        volver.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                addArticuloStage.close();
+                articuloStage.show();
+            }
+        });
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(titulo, volver);
+        vbox.setAlignment(Pos.CENTER);
+        root.getChildren().add(vbox);
+        
+        Scene scene = new Scene(root, 300, 250);
+        addArticuloStage.setScene(scene);
+        addArticuloStage.setTitle("Añadir articulo");
+        addArticuloStage.show();
+    }
+    
+    private void pantallaShowArticulos() {
+        Stage showArticuloStage = new Stage();
+        StackPane root = new StackPane();
+        Text titulo = new Text(30, 30, "Lista de articulos");
+        titulo.setStyle("-fx-font: 15 arial;");
+        
+        Button volver = new Button();
+        volver.setText("Volver");
+        volver.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                showArticuloStage.close();
+                articuloStage.show();
+            }
+        });
+        
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(titulo, volver);
+        vbox.setAlignment(Pos.CENTER);
+        root.getChildren().add(vbox);
+        
+        Scene scene = new Scene(root, 300, 250);
+        showArticuloStage.setScene(scene);
+        showArticuloStage.setTitle("Mostrar articulos");
+        showArticuloStage.show();
+    }
+    
+       private void pantallaDeleteArticulo() {
+        Stage deleteArticuloStage = new Stage();
+        StackPane root = new StackPane();
+        Text titulo = new Text(30, 30, "Eliminar articulo");
+        titulo.setStyle("-fx-font: 15 arial;");
+        
+        Button volver = new Button();
+        volver.setText("Volver");
+        volver.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                deleteArticuloStage.close();
+                articuloStage.show();
+            }
+        });
+        
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(titulo, volver);
+        vbox.setAlignment(Pos.CENTER);
+        root.getChildren().add(vbox);
+        
+        Scene scene = new Scene(root, 300, 250);
+        deleteArticuloStage.setScene(scene);
+        deleteArticuloStage.setTitle("Eliminar articulo");
+        deleteArticuloStage.show();
     }
     
 }
--- producto2_POO_BBDD/src/grupofp/vista/FXCliente.java
+++ producto2_POO_BBDD/src/grupofp/vista/FXCliente.java
@@ -6,9 +6,12 @@ package grupofp.vista;
 
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 /**
@@ -16,13 +19,46 @@ import javafx.stage.Stage;
  * @author joanb
  */
 public class FXCliente extends FXMain {
-    Stage clienteStage = new Stage();
+  Stage clienteStage = new Stage();
 
     public void start(Stage mainMenu) {
 
-        Button btn = new Button();
-        btn.setText("Cliente");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
+        Text titulo = new Text(30, 30, "¿Que deseas hacer?");
+        titulo.setStyle("-fx-font: 15 arial;");
+        
+        Button addCliente = new Button();
+        addCliente.setText("Añadir cliente");
+        addCliente.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                addClienteStage();
+            }
+        });
+        
+        Button showClientes = new Button();
+        showClientes.setText("Mostrar clientes");
+        showClientes.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                showClientesEscena();
+            }
+        });
+        
+        Button deleteCliente = new Button();
+        deleteCliente.setText("Eliminar cliente");
+        deleteCliente.setOnAction(new EventHandler<ActionEvent>() {
+            
+            @Override
+            public void handle(ActionEvent event) {
+                deleteClienteStage();
+            }
+        });
+        
+        Button volver = new Button();
+        volver.setText("Volver");
+        volver.setOnAction(new EventHandler<ActionEvent>() {
             
             @Override
             public void handle(ActionEvent event) {
@@ -32,13 +68,55 @@ public class FXCliente extends FXMain {
         });
         
         StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        VBox vbox = new VBox(10);
+        vbox.getChildren().addAll(titulo, addCliente, showClientes, deleteCliente, volver);
+        vbox.setAlignment(Pos.CENTER);
+        root.getChildren().add(vbox);
         
         Scene scene = new Scene(root, 300, 250);
         
-        clienteStage.setTitle("Hello World!");
+        clienteStage.setTitle("Clientes");
         clienteStage.setScene(scene);
         clienteStage.show();
+    }
+    
+    private void addClienteStage() {
+        Stage addClienteStage = new Stage();
+        StackPane root = new StackPane();
+        Text titulo = new Text(30, 30, "Añadir cliente");
+        titulo.setStyle("-fx-font: 15 arial;");
+        root.getChildren().add(titulo);
+        
+        Scene scene = new Scene(root, 300, 250);
+        addClienteStage.setScene(scene);
+        addClienteStage.setTitle("Añadir cliente");
+        addClienteStage.show();
+    }
+    
+    private void showClientesEscena() {
+        Stage showClientesStage = new Stage();
+        StackPane root = new StackPane();
+        Text titulo = new Text(30, 30, "Lista de clientes");
+        titulo.setStyle("-fx-font: 15 arial;");
+        root.getChildren().add(titulo);
+        
+        Scene scene = new Scene(root, 300, 250);
+        showClientesStage.setScene(scene);
+        showClientesStage.setTitle("Mostrar clientes");
+        showClientesStage.show();
+    }
+    
+       private void deleteClienteStage() {
+        Stage deleteClienteStage = new Stage();
+        StackPane root = new StackPane();
+        Text titulo = new Text(30, 30, "Eliminar cliente");
+        titulo.setStyle("-fx-font: 15 arial;");
+        root.getChildren().add(titulo);
+        
+        Scene scene = new Scene(root, 300, 250);
+        deleteClienteStage.setScene(scene);
+        deleteClienteStage.setTitle("Eliminar cliente");
+        deleteClienteStage.show();
     }
     
 }
