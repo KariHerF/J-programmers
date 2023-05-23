80564af2407782686c8ae58299045d01b5038edd
Joan Bogunya Salvans <joanbogunya98@gmail.com>
2023-05-23 21:45:01 +0200

    add articulo conectado con DB

--- producto2_POO_BBDD/src/grupofp/vista/FXArticulo.java
+++ producto2_POO_BBDD/src/grupofp/vista/FXArticulo.java
@@ -1,10 +1,18 @@
 package grupofp.vista;
 
+import grupofp.controlador.Controlador;
+import grupofp.controlador.Main;
+import java.time.Duration;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
@@ -15,17 +23,47 @@ import javafx.stage.Stage;
  * @author joanb
  */
 public class FXArticulo extends FXMain {
-    Stage articuloStage = new Stage();
+
+    Stage articuloStage = new Stage();    
+
+    private void addArticulo(String codigo, String descripcion, String pvp, String tiempoPrep,
+            String gastosEnvio, Stage stage) {
+        try {
+            String vacio = "";
+            if (vacio.matches(codigo) || vacio.matches(descripcion)
+                    || vacio.matches(pvp) || vacio.matches(tiempoPrep) || vacio.matches(gastosEnvio)) {
+                mostrarAlerta("error", "Debes llenar todos los campos");
+            } else {
+                float pvpFloat = Float.parseFloat(pvp);
+                Duration duracion = Duration.parse(tiempoPrep);
+                float gastosEnvioFloat = Float.parseFloat(gastosEnvio);
+                System.out.println("before");
+
+                this.miControlador.crearArticulo(codigo, descripcion, pvpFloat,
+                        duracion, gastosEnvioFloat);
+                System.out.println("after");
+                stage.close();
+                mostrarAlerta("success", "Se ha creado el articulo correctamente");
+                articuloStage.show();
+            }
+
+        } catch (Exception ex) {
+            System.out.println(ex);
+            mostrarAlerta("error", ex.getMessage());
+        }
+    }
 
     public void start(Stage mainMenu) {
-
+        if (this.miControlador == null) {
+            System.out.println("null inside");
+        }
         Text titulo = new Text(30, 30, "¿Que deseas hacer?");
         titulo.setStyle("-fx-font: 15 arial;");
-        
+
         Button addArticulo = new Button();
         addArticulo.setText("Añadir articulo");
         addArticulo.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 pantallaAddArticulo();
@@ -33,11 +71,11 @@ public class FXArticulo extends FXMain {
 
             }
         });
-        
+
         Button showArticulos = new Button();
         showArticulos.setText("Mostrar articulos");
         showArticulos.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 pantallaShowArticulos();
@@ -45,123 +83,177 @@ public class FXArticulo extends FXMain {
 
             }
         });
-        
+
         Button deleteArticulo = new Button();
         deleteArticulo.setText("Eliminar articulo");
         deleteArticulo.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 pantallaDeleteArticulo();
                 articuloStage.hide();
             }
         });
-        
+
         Button volver = new Button();
         volver.setText("Volver");
         volver.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 articuloStage.close();
                 mainMenu.show();
             }
         });
-        
+
         StackPane root = new StackPane();
         VBox vbox = new VBox(10);
         vbox.getChildren().addAll(titulo, addArticulo, showArticulos, deleteArticulo, volver);
         vbox.setAlignment(Pos.CENTER);
         root.getChildren().add(vbox);
-        
+
         Scene scene = new Scene(root, 300, 250);
-        
+
         articuloStage.setTitle("Articulos");
         articuloStage.setScene(scene);
         articuloStage.show();
     }
-    
+
     private void pantallaAddArticulo() {
         Stage addArticuloStage = new Stage();
         StackPane root = new StackPane();
         Text titulo = new Text(30, 30, "Añadir articulo");
         titulo.setStyle("-fx-font: 15 arial;");
-        
+
+        TextField codigoArticulo = new TextField();
+        codigoArticulo.setPromptText("Introduce el codigo");
+        Label labelCodigo = new Label("Codigo:");
+        HBox boxCodigo = new HBox(10);
+        boxCodigo.getChildren().addAll(labelCodigo, codigoArticulo);
+        boxCodigo.setAlignment(Pos.BASELINE_LEFT);
+
+        TextField descripcionArticulo = new TextField();
+        descripcionArticulo.setPromptText("Introduce la descripcion");
+        Label labelDescripcion = new Label("Descripcion:");
+        HBox boxDescripcion = new HBox(10);
+        boxDescripcion.getChildren().addAll(labelDescripcion, descripcionArticulo);
+        boxDescripcion.setAlignment(Pos.BASELINE_LEFT);
+
+        TextField pvpArticulo = new TextField();
+        pvpArticulo.setPromptText("Introduce un decimal");
+        Label labelPvp = new Label("PVP:");
+        HBox boxPvp = new HBox(10);
+        boxPvp.getChildren().addAll(labelPvp, pvpArticulo);
+        boxPvp.setAlignment(Pos.BASELINE_LEFT);
+
+        TextField tiempoPrepArticulo = new TextField();
+        tiempoPrepArticulo.setPromptText("Formato ISO 8601 (PTnHnMnS)");
+        Label labelTiempoPrep = new Label("Tiempo de Preparacion:");
+        HBox boxTiempoPrep = new HBox(10);
+        boxTiempoPrep.getChildren().addAll(labelTiempoPrep, tiempoPrepArticulo);
+        boxTiempoPrep.setAlignment(Pos.BASELINE_LEFT);
+
+        TextField gastosEnvioArticulo = new TextField();
+        gastosEnvioArticulo.setPromptText("Introduce un decimal");
+        Label labelGastosEnvio = new Label("Gastos Envio:");
+        HBox boxGastosEnvio = new HBox(10);
+        boxGastosEnvio.getChildren().addAll(labelGastosEnvio, gastosEnvioArticulo);
+        boxGastosEnvio.setAlignment(Pos.BASELINE_LEFT);
+
+        Button add = new Button();
+        add.setText("Añadir");
+        add.setOnAction(new EventHandler<ActionEvent>() {
+
+            @Override
+            public void handle(ActionEvent event) {
+                addArticulo(codigoArticulo.getText(), descripcionArticulo.getText(),
+                        pvpArticulo.getText(), tiempoPrepArticulo.getText(), gastosEnvioArticulo.getText(), addArticuloStage);
+
+            }
+        });
+
         Button volver = new Button();
         volver.setText("Volver");
         volver.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 addArticuloStage.close();
                 articuloStage.show();
             }
         });
+
+        HBox boxBotones = new HBox(20);
+        boxBotones.getChildren().addAll(add, volver);
+        boxBotones.setAlignment(Pos.CENTER);
+        boxBotones.setPadding(new Insets(20, 20, 0, 0));
+
         VBox vbox = new VBox(10);
-        vbox.getChildren().addAll(titulo, volver);
+        vbox.getChildren().addAll(titulo, boxCodigo, boxDescripcion, boxPvp, boxTiempoPrep, boxGastosEnvio, boxBotones);
         vbox.setAlignment(Pos.CENTER);
+        vbox.setPadding(new Insets(20, 20, 20, 20));
         root.getChildren().add(vbox);
-        
-        Scene scene = new Scene(root, 300, 250);
+
+        Scene scene = new Scene(root, 400, 300);
         addArticuloStage.setScene(scene);
         addArticuloStage.setTitle("Añadir articulo");
         addArticuloStage.show();
     }
-    
+
     private void pantallaShowArticulos() {
         Stage showArticuloStage = new Stage();
         StackPane root = new StackPane();
         Text titulo = new Text(30, 30, "Lista de articulos");
         titulo.setStyle("-fx-font: 15 arial;");
-        
+
         Button volver = new Button();
         volver.setText("Volver");
         volver.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 showArticuloStage.close();
                 articuloStage.show();
             }
         });
-        
+
         VBox vbox = new VBox(10);
         vbox.getChildren().addAll(titulo, volver);
         vbox.setAlignment(Pos.CENTER);
         root.getChildren().add(vbox);
-        
+
         Scene scene = new Scene(root, 300, 250);
         showArticuloStage.setScene(scene);
         showArticuloStage.setTitle("Mostrar articulos");
         showArticuloStage.show();
     }
-    
-       private void pantallaDeleteArticulo() {
+
+    private void pantallaDeleteArticulo() {
         Stage deleteArticuloStage = new Stage();
         StackPane root = new StackPane();
         Text titulo = new Text(30, 30, "Eliminar articulo");
         titulo.setStyle("-fx-font: 15 arial;");
-        
+
         Button volver = new Button();
         volver.setText("Volver");
         volver.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 deleteArticuloStage.close();
                 articuloStage.show();
             }
         });
-        
+
         VBox vbox = new VBox(10);
         vbox.getChildren().addAll(titulo, volver);
         vbox.setAlignment(Pos.CENTER);
         root.getChildren().add(vbox);
-        
+
         Scene scene = new Scene(root, 300, 250);
         deleteArticuloStage.setScene(scene);
         deleteArticuloStage.setTitle("Eliminar articulo");
         deleteArticuloStage.show();
     }
-    
+
 }
--- producto2_POO_BBDD/src/grupofp/vista/FXMain.java
+++ producto2_POO_BBDD/src/grupofp/vista/FXMain.java
@@ -4,90 +4,104 @@
  */
 package grupofp.vista;
 
+import grupofp.controlador.Controlador;
+import grupofp.controlador.Main;
+import grupofp.modelo.Datos;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.control.Alert.AlertType;
 
 /**
  *
  * @author joanb
  */
 public class FXMain extends Application {
-    
+
+    Datos misDatos = new Datos();
+    GestionOS miVistaGestionOS = new GestionOS();
+    Controlador miControlador = new Controlador(miVistaGestionOS, misDatos);
+
+    private void iniciarApp() {
+        /* Se instancian las clases del MVC */
+         miVistaGestionOS.setControlador(miControlador);
+        misDatos.setControlador(miControlador);
+        
+    }
+
     @Override
     public void start(Stage mainMenu) {
-        
         Text titulo = new Text(30, 30, "¿Que deseas hacer?");
         titulo.setStyle("-fx-font: 16 arial;");
-        
+
         Button gestionarArticulos = new Button();
         gestionarArticulos.setText("Gestionar articulos");
         gestionarArticulos.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 FXArticulo fxArt = new FXArticulo();
-                
+
                 mainMenu.hide();
                 fxArt.start(mainMenu);
             }
         });
-        
+
         Button gestionarClientes = new Button();
         gestionarClientes.setText("Gestionar clientes");
         gestionarClientes.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 FXCliente fxCliente = new FXCliente();
-                
+
                 mainMenu.hide();
                 fxCliente.start(mainMenu);
             }
         });
-        
+
         Button gestionarPedidos = new Button();
         gestionarPedidos.setText("Gestionar pedidos");
         gestionarPedidos.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 FXPedido fxPedido = new FXPedido();
-                
+
                 mainMenu.hide();
                 fxPedido.start(mainMenu);
             }
         });
-        
+
         Button salir = new Button();
         salir.setText("Salir");
         salir.setOnAction(new EventHandler<ActionEvent>() {
-            
+
             @Override
             public void handle(ActionEvent event) {
                 Platform.exit();
                 System.exit(0);
             }
         });
-        
+
         VBox vbox = new VBox(10);
         vbox.getChildren().addAll(titulo, gestionarArticulos, gestionarClientes, gestionarPedidos, salir);
         vbox.setAlignment(Pos.CENTER);
-        
+
         StackPane root = new StackPane();
         root.getChildren().add(vbox);
 
-        
         Scene scene = new Scene(root, 600, 500);
-        
+
         mainMenu.setTitle("Producto 5");
         mainMenu.setScene(scene);
         mainMenu.show();
@@ -97,7 +111,23 @@ public class FXMain extends Application {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        FXMain miPrincipal = new FXMain();
+        miPrincipal.iniciarApp();
+        miPrincipal.launch(args);
+
     }
-    
+
+    public void mostrarAlerta(String tipo, String message) {
+        if (tipo == "error") {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setHeaderText("Ha ocurrido un error");
+            alert.setContentText(message);
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(AlertType.CONFIRMATION);
+            alert.setHeaderText(message);
+            alert.showAndWait();
+        }
+
+    }
 }
